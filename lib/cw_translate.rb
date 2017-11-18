7226da8a7778daf3d5fe3b4a0bb6d9baf5caee83
Alaa Sarhan <sarhan.alaa@gmail.com>
2017-11-18 09:21:49 +0100

    new line

--- lib/cw_translate.rb
+++ lib/cw_translate.rb
@@ -3,7 +3,8 @@ require 'cw_translate/translator'
 require 'cw_translate/cli'
 
 module CwTranslate
-  # @param config [CwTranslate::Config] optional. default load mechanism is ueed if config is not passed.
+  # @param config [CwTranslate::Config] optional. default load mechanism is ueed
+  # if config is not passed.
   # See CwTranslate::Config.load
   def self.init_translate(config = nil)
     config ||= Config.load
