3c042d84baec3d77b100520a3cde2d00de2d799d
Alaa Sarhan <sarhan.alaa@gmail.com>
2018-02-02 00:06:39 +0100

    remove redundant braces

--- spec/cw_translate/translator/yandex_spec.rb
+++ spec/cw_translate/translator/yandex_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe CwTranslate::Translators::Yandex do
   describe '#translate' do
     it 'translates using Yandex API successfully' do
       expect_any_instance_of(::Yandex::Translator).to receive(:translate)
-        .with('hello', {to: 'de', from: 'en'}) { 'hallo' }
+        .with('hello', to: 'de', from: 'en') { 'hallo' }
       expect(subject.translate('hello', 'de', 'en')).to eq 'hallo'
     end
 
     context 'even without source lang' do
       it 'translates using Yandex API successfully' do
         expect_any_instance_of(::Yandex::Translator).to receive(:translate)
-          .with('hello', {to: 'de', from: nil}) { 'hallo' }
+          .with('hello', to: 'de', from: nil) { 'hallo' }
         expect(subject.translate('hello', 'de')).to eq 'hallo'
       end
     end
